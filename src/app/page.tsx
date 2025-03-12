60b6cb8988d0243789da552b4ffd1b7d41bd905b
Paul O'Connell <p_ollie@hotmail.com>
2025-03-12 16:22:26 -0700

    Milestone: new next.js project from scratch, got google API key and it's running

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { GoogleGenerativeAI } from"@google/generative-ai";
+import React, { useState } from 'react';
 
 export default function Home() {
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  
+//console.log("check key", apiKey)
+const genAI = new GoogleGenerativeAI(apiKey);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const [aiResponse, setAiResponse] = useState('');
+const [prompt, setPrompt] = useState('');
+
+const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setPrompt(event.target.value);
+};
+
+const handleClick = async()=>{
+  
+ // const prompt = "Explain how AI works";
+  //console.log("got prompt", prompt)
+  const result = await model.generateContent(prompt);
+ // console.log("got response",Object.keys(result.response));
+  setAiResponse(() => result.response.text());
+}
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -20,6 +48,11 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
+<div><input value={prompt} onChange={handleInputChange}></input></div>
+            <div>
+              <button onClick={handleClick}>Click me to get AI response</button>
+              {aiResponse}
+            </div>
         <div className={styles.ctas}>
           <a
             className={styles.primary}
